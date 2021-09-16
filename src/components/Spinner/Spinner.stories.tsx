ba7c7fb73f465a08e481db07d54f1c3f5f99d555
akshataw <wagheakshu@gmail.com>
2021-09-16 06:57:29 +0000

    Update: Spinner component

---
+++ src/components/Spinner/Spinner.stories.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import Spinner, { SpinnerProps } from '.';
+import styled from 'styled-components';
+
+export default {
+    title: 'Components/Spinner',
+    component: Spinner,
+} as Meta;
+
+const Template: Story<SpinnerProps> = (args) => <Spinner {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+    size: 50
+}
+
+const DarkBackground = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 50px;
+    height: 50px;
+    background-color: #2c2c59;
+`;
+
+export const Light: React.FC = () => (
+    <DarkBackground>
+        <Spinner size={50} light />
+    </DarkBackground>
+)
